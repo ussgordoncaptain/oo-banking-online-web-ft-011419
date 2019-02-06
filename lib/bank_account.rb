f270cc1008fd34d947777bffd4dc67f0364f99c2
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-02-06 21:55:52 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
 class BankAccount
-
+  def initialize(name)
+    @name = name
+    @status = "open"
+    @balence = 1000
+  end
+  attr_reader :name 
+  attr_accessor :status, :balence
+  def deposit(num)
+    @balence += num
+  end
+  def valid?
+    if @status == "closed" || @balence > 0 
+      return true 
+    else
+     return false 
+    end 
+   end
+   def close_account
+     @status = "closed"
+   end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
 class Transfer
   # your code here
+  def initialize(sender,reciver, num)
+    @sender = sender 
+    @receiver = reciver
+    @status = "pending"
+    @amount = num
+  end
+  attr_accessor :status
+  def execute_transaction 
+    if @status = "complete" || @status = "reversed"
+      return nil
+    end
+    if @amount > @sender.balence || @sender.status = "closed"  
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+    @sender.balence -= @amount 
+    @receiver.balance += @amount
+    @status = "complete"
+  end
+  def reverse_transfer
+    if @status = "complete"
+      @status = 
+    else
+      return nil
+    end
 end
